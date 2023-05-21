3e206e7ec287051c2fd84f784734be46d4a169c9
hannanel100 <hannanel100@gmail.com>
2023-05-21 15:05:32 +0300

    remove initial loading of users

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,4 @@
-<script lang="ts">
-	export let data;
-	console.log(data);
-	let { items: users } = data;
-</script>
-
 <h1>users</h1>
 
-<p>
-	{#if !users}
-		<p>no users</p>
-	{:else if users}
-		{#each users as user}
-			<p>{user.email}</p>
-		{/each}
-	{/if}
-</p>
+<!-- got to users page -->
+<a href="/users">Go to users page</a>
