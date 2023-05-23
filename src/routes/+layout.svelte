996d4c503f7dc483a425b6bd98f25e6d2189926e
hannanel100 <hannanel100@gmail.com>
2023-05-23 14:17:44 +0300

    feat: :sparkles: added page transition animations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script>
 	import '@fontsource/roboto';
 	import Header from '../Header.svelte';
+	import { fly } from 'svelte/transition';
+	export let data;
 </script>
 
 <div>
 	<Header />
 	<main>
-		<slot />
+		{#key data.url}
+			<div in:fly={{ x: -200, duration: 300, delay: 300 }} out:fly={{ x: 200, duration: 300 }}>
+				<slot />
+			</div>
+		{/key}
 	</main>
 </div>
 
