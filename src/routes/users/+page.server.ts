36eb842ee036c35e0f3010a9f166db25042569e8
hannanel100 <hannanel100@gmail.com>
2023-05-23 11:52:12 +0300

    fix: :label: updated type on page.server.ts for users route

--- src/routes/users/+page.server.ts
+++ src/routes/users/+page.server.ts
@@ -1,12 +1,11 @@
 import { fetchUsers } from '../../lib/fetchUsers';
 
-import type { PageLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
 export const load = (async ({ fetch, url }) => {
-	console.log('🚀 ~ file: +page.server.ts:6 ~ load ~ url:', url.searchParams.get('limit'));
 	const limit = Number(url.searchParams.get('limit')) || 10;
 	const page = Number(url.searchParams.get('page')) || 0;
 
 	const data = await fetchUsers(limit, page, fetch);
 	return data;
-}) satisfies PageLoad;
+}) satisfies PageServerLoad;
