5fb8b0a65862154fa84b7674e53bbcb8528f5028
hannanel100 <hannanel100@gmail.com>
2023-05-29 16:28:05 +0300

    feat: :goal_net: adding error pages - WIP

--- src/routes/users/+page.server.ts
+++ src/routes/users/+page.server.ts
@@ -4,8 +4,9 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async ({ fetch, url }) => {
 	const limit = Number(url.searchParams.get('limit')) || 10;
-
+	if (limit > 100) throw new Error('Limit must be less than 100');
 	const page = Number(url.searchParams.get('page')) || 0;
+	if (page < 0) throw new Error('Page must be greater than 0');
 
 	const sort = url.searchParams.get('sort') || 'email:desc';
 
---
+++ src/routes/users/[slug]/+error.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { page } from '$app/stores';
+	$: console.log($page.status);
+</script>
+
+<h1>{$page.status}: {$page.error?.message}</h1>
